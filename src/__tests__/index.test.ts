d91fe60027eea3f7b4cb0fb2642054ecdd2da204
hebertcisco <mrrabbitomg@gmail.com>
2021-06-07 17:45:10 -0300

    fix tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,6 +2,5 @@ import pensador from '../index';
 
 it('Works', async () => {
   const result = await pensador({ term: 'Elon Musk', max: 5 });
-
   expect(result.searchTerm).toBe('frases_de_elon_musk');
 });
