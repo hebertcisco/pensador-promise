e3578575adfc37a049e1a63bbcdaf9ebd8813e19
hebertcisco <mrrabbitomg@gmail.com>
2021-06-08 04:35:05 +0000

    add next example

---
+++ examples/pensador-next/pages/index.js
@@ -0,0 +1,94 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import axios from "axios";
+
+export default function Home({ phrases }) {
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    const randonPhrase = randomInt(0, 10);
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Pensador API</title>
+                <meta name="description" content="Frases vindas do Pensador" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Pensador API
+                </h1>
+
+                <p className={styles.description}>
+                    Como disse {phrases.frases[randonPhrase].autor}{' '}
+                    <code className={styles.code}>{JSON.stringify(phrases.frases[randonPhrase].texto)}</code>
+                </p>
+
+                <div className={styles.grid}>
+                    <a href="https://github.com/hebertcisco/pensador-api#readme" className={styles.card}>
+                        <h2>Documentação &rarr;</h2>
+                        <p>Aprenda como integrar a API em seu projeto</p>
+                    </a>
+
+                    <a href="https://www.npmjs.com/package/pensador-promise" className={styles.card}>
+                        <h2>Pacote NPM &rarr;</h2>
+                        <p>Instale o Pacote NPM em seu projeto</p>
+                    </a>
+
+                    <a
+                        href="https://github.com/hebertcisco/pensador-promise"
+                        className={styles.card}
+                    >
+                        <h2>Código Fonte &rarr;</h2>
+                        <p>Veja como funciona e contribuia com o projeto</p>
+                    </a>
+
+                    <a
+                        href="https://pensador-api.vercel.app/?term=Jesus+Cristo&max=7"
+                        className={styles.card}
+                    >
+                        <h2>Endpoint &rarr;</h2>
+                        <p>
+                            Teste o Endpoint da API que retorna um JSON de frases
+            </p>
+                    </a>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://hebertbarros.me?utm_source=pensador-promise&utm_campaign=pensador-api"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        HB.me
+                    </span>
+                </a>
+            </footer>
+        </div>
+    )
+}
+
+export const getStaticProps = async ({ params }) => {
+    try {
+        const result = await axios({
+            method: "get",
+            url: `https://pensador-api.vercel.app`
+        });
+        const data = result.data;
+
+        return {
+            props: { phrases: data },
+            revalidate: 160
+        };
+    } catch (err) {
+        return {
+            props: { phrases: { error: err?.message } },
+            revalidate: 160
+        };
+    }
+};
