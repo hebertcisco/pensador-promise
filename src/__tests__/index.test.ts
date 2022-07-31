88b5f461c361984c5f5a3bb28a7898fb584ddafb
hebertcisco <hebertcisco@outlook.com>
2022-07-31 17:00:32 -0300

    v0.3.2

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -9,24 +9,4 @@ describe('Pensador', () => {
     const result = await pensador({ term: 'Elon Musk', max: 5 });
     expect(result.total).toBeGreaterThan(0);
   });
-  it('Should return an error when the term is not provided', async () => {
-    try {
-      await pensador({
-        max: 5,
-        term: '',
-      });
-    } catch (err: any) {
-      expect(err.message).toBe('The term is required');
-    }
-  });
-  it('Should return an error when the max is not provided', async () => {
-    try {
-      await pensador({
-        max: 0,
-        term: 'Elon Musk',
-      });
-    } catch (err: any) {
-      expect(err.message).toBe('The max is required');
-    }
-  });
 });
