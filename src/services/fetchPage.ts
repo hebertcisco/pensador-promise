31da05d7a96cfdaf738a040bbb1c949d2105830e
hebertcisco <hebertcisco@outlook.com>
2022-07-31 16:39:01 -0300

    fix: :construction: Fixed issue when returning authors phrases by search

--- src/services/fetchPage.ts
+++ src/services/fetchPage.ts
@@ -1,13 +1,27 @@
 import iconv from 'iconv-lite';
 import { request } from 'undici';
+import qs from 'qs';
+import { isSuccessfulRequest } from '../utils/isSuccessfulRequest';
 
-async function fetchPage(searchTerm: string, current = 1, baseUrl: string) {
-  return new Promise((resolve, reject) => {
-    return request(`${baseUrl}/${searchTerm}/${current}`)
-      .then((res) => res.body.arrayBuffer())
-      .then((arrayBuffer) => iconv.decode(Buffer.from(arrayBuffer), 'utf-8').toString())
-      .then((body) => resolve(body))
-      .catch((err) => reject(err));
+export async function fetchPage(searchTerm: string, current = 1, baseUrl: string) {
+  const params = qs.stringify({
+    q: searchTerm,
+    p: current,
   });
+  try {
+    const res = await request(`${baseUrl}/busca.php?${params}`);
+    if (!isSuccessfulRequest(res.statusCode)) {
+      throw new Error(`Error fetching ${searchTerm}, status code: ${res.statusCode}`);
+    }
+    const arrayBuffer = await res.body.arrayBuffer();
+    const body = iconv.decode(Buffer.from(arrayBuffer), 'utf-8').toString();
+    return body;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw error;
+    }
+    throw new Error(`Error fetching ${searchTerm}`);
+  }
 }
+
 export default fetchPage;
